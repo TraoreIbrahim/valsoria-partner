4527f116bae75a252421960a0cc9dae0c3c69989
TraoreIbrahim <traore.oscar009@gmail.com>
2023-07-28 15:38:28 +0000

    fix bug

---
+++ build/precache-manifest.2af66de02ceab50fb05f8baf50bbbdbb.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2afc53b6932e90e1ec9016b1e33fe2e6",
+    "url": "/valsoria-partner/index.html"
+  },
+  {
+    "revision": "85f874bc47aff9bac15c",
+    "url": "/valsoria-partner/static/css/2.fea3aa44.chunk.css"
+  },
+  {
+    "revision": "f037afdfe2bb64810ff2",
+    "url": "/valsoria-partner/static/css/main.ed5a0449.chunk.css"
+  },
+  {
+    "revision": "85f874bc47aff9bac15c",
+    "url": "/valsoria-partner/static/js/2.001b3058.chunk.js"
+  },
+  {
+    "revision": "90b29c0b4ec4b50f507746c977b25f84",
+    "url": "/valsoria-partner/static/js/2.001b3058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f037afdfe2bb64810ff2",
+    "url": "/valsoria-partner/static/js/main.9a4d3ad5.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/valsoria-partner/static/js/main.9a4d3ad5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e61c11543738697ba1dc",
+    "url": "/valsoria-partner/static/js/runtime-main.66b9757b.js"
+  },
+  {
+    "revision": "85064a97348166467bd7bc2ad80ccdce",
+    "url": "/valsoria-partner/static/media/404.85064a97.svg"
+  },
+  {
+    "revision": "a0320da1a9ae65d73f6af7780883975c",
+    "url": "/valsoria-partner/static/media/500.a0320da1.svg"
+  },
+  {
+    "revision": "ed3cd4b507f8209412bc426332f23589",
+    "url": "/valsoria-partner/static/media/canada.ed3cd4b5.svg"
+  },
+  {
+    "revision": "0e5039f2e96dc9f03159d8c411bbfc95",
+    "url": "/valsoria-partner/static/media/compagny _logo.0e5039f2.jpg"
+  },
+  {
+    "revision": "968aaa24eeffdd779e4fc9c27c5cb730",
+    "url": "/valsoria-partner/static/media/france.968aaa24.svg"
+  },
+  {
+    "revision": "d810f62137d64a0a58035c80489b7d54",
+    "url": "/valsoria-partner/static/media/germany.d810f621.svg"
+  },
+  {
+    "revision": "612e617f5d72975e63e53d793d757785",
+    "url": "/valsoria-partner/static/media/italy.612e617f.svg"
+  },
+  {
+    "revision": "1f905d23af144bc76e488f00d516de2e",
+    "url": "/valsoria-partner/static/media/japan.1f905d23.svg"
+  },
+  {
+    "revision": "d1cfd33f84523d5c9da5f5e43e33ee6b",
+    "url": "/valsoria-partner/static/media/logo_valsoria.d1cfd33f.png"
+  },
+  {
+    "revision": "b95351ff37500f2ed989ac26dcfd8a82",
+    "url": "/valsoria-partner/static/media/momo.b95351ff.jpg"
+  },
+  {
+    "revision": "ed11b5dc0d91566e4bd64e5eab13917e",
+    "url": "/valsoria-partner/static/media/om.ed11b5dc.png"
+  },
+  {
+    "revision": "9992e0ee061c844b089dd94784cf7e9d",
+    "url": "/valsoria-partner/static/media/profile-cover.9992e0ee.jpg"
+  },
+  {
+    "revision": "4d26e151b224eaa88df2742351073c56",
+    "url": "/valsoria-partner/static/media/profile-picture-1.4d26e151.jpg"
+  },
+  {
+    "revision": "487133c5319652023f459ea4e3a4720a",
+    "url": "/valsoria-partner/static/media/profile-picture-2.487133c5.jpg"
+  },
+  {
+    "revision": "9a250e5ca0df93fd467f77c4d7e3d3d4",
+    "url": "/valsoria-partner/static/media/profile-picture-3.9a250e5c.jpg"
+  },
+  {
+    "revision": "ccd796b31c6ca0c778c6065b3b6a20b7",
+    "url": "/valsoria-partner/static/media/profile-picture-4.ccd796b3.jpg"
+  },
+  {
+    "revision": "58ee1d5a0a93d00df3f6c12170ebd3ff",
+    "url": "/valsoria-partner/static/media/signin.58ee1d5a.svg"
+  },
+  {
+    "revision": "36ab476e5e55f496749ee61897a9cfb5",
+    "url": "/valsoria-partner/static/media/united-states-of-america.36ab476e.svg"
+  }
+]);
